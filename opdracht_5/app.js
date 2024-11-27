13465d8491e6bbd9c83500407ea3eb93d2c4e3da
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2024-11-27 09:51:28 +0100

    update

---
+++ opdracht_5/app.js
@@ -0,0 +1,6 @@
+let verlanglijst = ["playstation 5", "pc", "geld", "auto", "laptop"];
+
+
+for(let i = 0; i <verlanglijst.length; i++)[
+   document.getElementById('output').innerHTML += verlanglijst[i] + '<br>'
+]
