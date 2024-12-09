fffde0cda6338420e3cace6327fa3204ed7fbeba
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2024-12-09 10:45:44 +0100

    .

---
+++ opdracht_4/app.js
@@ -0,0 +1,14 @@
+//let getal = 10 
+
+//for(let i = 1; i < 10; i++){
+    //document.getElementById('output').innerHTML += i + ' x ' + getal + ' = ' + i * getal + '<br>'
+//}
+
+function tafel_van(getal){
+    for(let i = 1; i < 11; i++)
+        document.getElementById('output').innerHTML += i + ' x ' + getal + ' = ' + i * getal + '<br>'    
+
+}
+
+tafel_van(7)
+
--- winkelmand/app.js
+++ winkelmand/app.js
@@ -1,11 +1,45 @@
-let winkelmand = [];
+let winkelmand = ["Bananen", "Whiskey", "Chips", "Bonen", "Aardappelen"];
 
-winkelmand.push("Bananen");
-winkelmand.push("Whiskey")
-winkelmand.push("Chips")
-winkelmand.push("Bonen")
-winkelmand.push("Aardappelen")
+let aantalProducten = winkelmand.length;
 
-console.log(fruit.length);
+document.getElementById("opdr1").innerText = `Er zitten ${aantalProducten} producten in uw winkelmand.`;
 
+document.getElementById("opdr2").innerText = `Uw producten zijn: ${winkelmand}`;
+
+document.getElementById("opdr3").innerText = `${winkelmand}, staat op de vierde plek in uw winkelmand`;
+
+winkelmand[1]= "Bier "
+
+document.getElementById("opdr4").innerText = `${winkelmand}`;
+
+function product_toevoegen(){
+    winkelmand.push("Aardbeien")
+    document.getElementById("opdr5").innerText = `${winkelmand}`
+}
+
+if (winkelmand.length >= 0) {
+    document.getElementById("opdr6").innerText = `U kunt boodschappen gaan doen.`;
+
+}  else {
+    document.getElementById("opdr6").innerText = `U heeft niets te kopen.`;
+}
+
+if (winkelmand[4] == "Drop") {
+    document.getElementById("opdr7").innerText = winkelmand;
+
+}  else {
+    document.getElementById("opdr7").innerText = `U heeft geen drop`;
+}
+
+let winkelmandopdr8 = ["Bananen", "Whiskey", "Chips", "Bonen", "Aardappelen"];
+document.getElementById("opdr8").innerHTML = winkelmandopdr8.join(" ");
+
+let winkelmandopdr9 = ["Bananen", "Whiskey", "Chips", "Bonen", "Aardappelen"];
+document.getElementById("opdr9").innerText = `${winkelmandopdr9}`
+winkelmandopdr9.splice (0,2)
+document.getElementById("opdr9.5").innerText = `${winkelmandopdr9}`
+
+let winkelmandopdr10 = ["Bananen", "Whiskey", "Chips", "Bonen", "Aardappelen"];
+winkelmandopdr10.sort();
+document.getElementById("opdr10").innerText = `${winkelmandopdr10}`
 
