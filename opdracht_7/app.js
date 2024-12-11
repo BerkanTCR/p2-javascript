e463bc5cd78918093e1ba7044b3c0af46e17618c
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2024-12-11 09:47:30 +0100

    .

---
+++ opdracht_7/app.js
@@ -0,0 +1,10 @@
+let legediv = document.getElementById("legedivID");
+let position = 0;
+
+let intervalId = setInterval(function() {
+    position+=10
+    legediv.style.left = position+"px"
+    if(position >innerWidth){
+        position = 0
+    }
+}, 100);
