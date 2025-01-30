f1a1cc598d72b0e11289b1b9af6b1219777e7a41
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2025-01-30 16:05:20 +0100

    opdracht_4 van Berkan Javascript

--- opdracht_4/app.js
+++ opdracht_4/app.js
@@ -12,3 +12,21 @@ function tafel_van(getal){
 
 tafel_van(7)
 
+function tafel_van(getal) {
+    let output = document.getElementById('output');
+    output.innerHTML = ''; 
+    for (let i = 1; i <= 10; i++) {
+        output.innerHTML += i + ' x ' + getal + ' = ' + i * getal + '<br>';
+    }
+}
+
+function updateTafel() {
+    let getal = document.getElementById('getal').value; 
+    if (getal) {
+        tafel_van(getal); 
+    } else {
+        alert("Vul een getal in!"); 
+    }
+}
+
+
